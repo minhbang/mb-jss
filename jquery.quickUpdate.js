a167cc9d875cf3c4b0a12cdeb863bfc20b5b678d
Minh Bang <contact@minhbang.com>
2015-10-16 11:07:19 +0700

    update selectize tags and quick update

--- jquery.quickUpdate.js
+++ jquery.quickUpdate.js
@@ -3,104 +3,141 @@
  */
 (function ($, window) {
     'use strict';
-    $.fn.extend({
-        quickUpdate: function (options) {
-            var defaults = {
-                url: null,
-                container: 'body',
-                placement: 'right',
-                title: 'Quick Update',
-                no_value: '--- Chưa cập nhật ---',
-                attribute: 'attribute',
-                element: '<input class="form-control _value" type="text" value="" name="_value">',
-                class: null,
-                dataTable: null,
-                updateSuccess: function (data, oTable, processResult) {
-                    if (data.type == 'success') {
-                        if (oTable) {
-                            oTable.dataTable().fnReloadAjax();
-                        }
-                        if (processResult && (typeof data.result !== 'undefined')) {
-                            processResult(data.result);
-                        }
-                    }
-                    $.fn.mbHelpers.showMessage(data.type, data.message);
-                },
-                processResult: null,
-                afterShow: null
-            };
-            options = $.extend(defaults, options);
-
-            function formHtml(attribute, elementClass) {
-                var element = options.element;
-                if (elementClass) {
-                    element = element.replace('class="', 'class="' + elementClass + ' ');
+    var defaults = {
+        url: null,
+        container: 'body',
+        placement: 'right',
+        title: 'Quick Update',
+        no_value: '--- Chưa cập nhật ---',
+        attribute: 'attribute',
+        elementTemplate: '<input class="form-control _value" type="text" value="" name="_value">',
+        elementClass: null,
+        dataTable: null,
+        updateParams: {_token: window.csrf_token},
+        updateSuccess: function (data, oTable, processResult) {
+            if (data.type == 'success') {
+                if (oTable) {
+                    oTable.dataTable().fnReloadAjax();
                 }
-                return '<form class="form-inline form-quick-update form-update-' + attribute + '">' +
-                    '<input type="hidden" value="' + attribute + '" name="_attr" class="_attr">' +
-                    '<div class="form-group">' +
-                    element +
-                    '<button class="btn btn-success btn-update-' + attribute + '-ok" type="button"><span class="glyphicon glyphicon-ok"></span></button>' +
-                    '<button class="btn btn-white btn-update-' + attribute + '-cancel" type="button"><span class="glyphicon glyphicon-remove"></span></button>' +
-                    '</div>' +
-                    '</form>';
-            }
-
-            var popover;
-            var container = $(options.container);
-
-            function hidePopover() {
-                if (popover) {
-                    popover.popover('hide');
-                    popover = null;
+                if (processResult && (typeof data.result !== 'undefined')) {
+                    processResult(data.result);
                 }
             }
+            $.fn.mbHelpers.showMessage(data.type, data.message);
+        },
+        processResult: null,
+        beforeSubmit: null,
+        afterShow: null
+    };
 
-            return this.each(function () {
-                var attribute = $(this).data('qu_attr') || options.attribute;
+    function formHtml(attribute, elementTemplate, elementClass) {
+        var element = elementTemplate;
+        if (elementClass) {
+            element = element.replace('class="', 'class="' + elementClass + ' ');
+        }
+        return '<form class="form-inline form-quick-update form-update-' + attribute + '">' +
+            '<input type="hidden" value="' + attribute + '" name="_attr" class="_attr">' +
+            '<div class="form-group">' +
+            element +
+            '<button class="btn btn-success btn-update-' + attribute + '-ok" type="button"><span class="glyphicon glyphicon-ok"></span></button>' +
+            '<button class="btn btn-white btn-update-' + attribute + '-cancel" type="button"><span class="glyphicon glyphicon-remove"></span></button>' +
+            '</div>' +
+            '</form>';
+    }
 
-                $(this).popover({
-                    container: options.container,
-                    placement: $(this).data('qu_placement') || options.placement,
-                    trigger: 'manual',
-                    html: true,
-                    title: $(this).data('qu_title') || options.title,
-                    content: formHtml(attribute, $(this).data('qu_class') || options.class)
-                }).click(function (e) {
-                    e.preventDefault();
-                    var old_val = $(this).data('qu_value');
-                    hidePopover();
-                    $(this).popover('show');
-                    popover = $(this);
-                    var form = container.find('.popover-content form');
-                    form.find('[name="_value"]').val(old_val);
-                    form.on('click', '.btn-update-' + attribute + '-cancel', function () {
-                        hidePopover();
-                    });
-                    form.on('click', '.btn-update-' + attribute + '-ok', function () {
-                        if (form.find('[name="_value"]').val() != old_val) {
-                            var url = popover.attr('href');
-                            if (options.url) {
-                                url = options.url.replace('__ID__', url.replace('#', ''));
+    function QuickUpdate(element, options) {
+        this.element = $(element);
+        this.options = $.extend(true, defaults, options);
+        this.container = $(this.options.container);
+        this.attribute = this.element.data('qu_attr') || this.options.attribute;
+        this.init();
+    }
+
+    QuickUpdate.prototype = {
+        init: function () {
+            var that = this;
+
+            that.element.popover({
+                container: that.options.container,
+                placement: that.element.data('qu_placement') || that.options.placement,
+                trigger: 'manual',
+                html: true,
+                title: that.element.data('qu_title') || that.options.title,
+                content: formHtml(
+                    that.attribute,
+                    that.options.elementTemplate,
+                    that.element.data('qu_class') || that.options.elementClass
+                )
+            });
+
+            that.element.click(function (e) {
+                if(that.element.hasClass('popover-showed')){
+                    return;
+                }
+                e.preventDefault();
+                var old_val = that.element.data('qu_value');
+                that.hideAll();
+                that.element.addClass('popover-showed').popover('show');
+                var form = that.container.find('.popover-content form');
+                form.find('[name="_value"]').val(old_val);
+
+                form.on('click', '.btn-update-' + that.attribute + '-cancel', function () {
+                    that.hideAll();
+                });
+
+                form.on('click', '.btn-update-' + that.attribute + '-ok', function () {
+                    var new_val = form.find('[name="_value"]').val();
+                    if (new_val != old_val) {
+                        if (that.options.beforeSubmit && !that.options.beforeSubmit(that.element, new_val)) {
+                            that.hideAll();
+                            return;
+                        }
+                        var url = that.element.attr('href');
+                        if (that.options.url) {
+                            url = that.options.url.replace('__ID__', url.replace('#', ''));
+                        }
+                        var params = container.find('.popover-content form').serialize();
+                        $.post(url + '?' + params, that.options.updateParams, function (data) {
+                            that.hideAll();
+                            if (that.options.updateSuccess) {
+                                that.options.updateSuccess(data, that.options.dataTable, that.options.processResult);
                             }
-                            var params = container.find('.popover-content form').serialize();
-                            $.post(url + '?' + params, {
-                                _token: window.csrf_token
-                            }, function (data) {
-                                hidePopover();
-                                if (options.updateSuccess) {
-                                    options.updateSuccess(data, options.dataTable, options.processResult);
-                                }
-                            }, 'json');
-                        } else {
-                            hidePopover();
-                        }
-                    });
-                    if (options.afterShow) {
-                        options.afterShow(this, form);
+                        }, 'json');
+                    } else {
+                        that.hideAll();
                     }
                 });
+                if (that.options.afterShow) {
+                    that.options.afterShow(that.element, form);
+                }
             });
+        },
+        show: function () {
+            this.element.popover('show');
+        },
+        hide: function () {
+            this.element.popover('hide');
+        },
+        hideAll: function () {
+            $('.popover-showed', this.container).removeClass('popover-showed').popover('hide');
         }
-    });
+    };
+
+    $.fn.quickUpdate = function (options) {
+        var lists = this,
+            retval = this;
+        lists.each(function () {
+            var plugin = $(this).data("quickUpdate");
+
+            if (!plugin) {
+                $(this).data("quickUpdate", new QuickUpdate(this, options));
+            } else {
+                if (typeof options === 'string' && typeof plugin[options] === 'function') {
+                    retval = plugin[options]();
+                }
+            }
+        });
+
+        return retval || lists;
+    };
 })(jQuery, window);
--- jquery.selectize_tags.js
+++ jquery.selectize_tags.js
@@ -5,47 +5,62 @@
  */
 (function ($) {
     'use strict';
-    $.fn.extend({
-        selectize_tags: function (settings) {
-            var defaults = {
-                plugins: ['remove_button'],
-                delimiter: ',',
-                persist: false,
-                options: '',
-                create: function (input) {
-                    return {
-                        value: input,
-                        text: input
-                    }
-                }
-            };
-
-            /**
-             * @param string opts
-             * @return Array
-             */
-            function buildOptions(opts) {
-                var options = [];
-                $.each(opts.split(','), function (i, value) {
-                    options.push({
-                        value: value,
-                        text: value
-                    });
-                });
-                return options;
+    var defaults = {
+        plugins: ['remove_button'],
+        delimiter: ',',
+        persist: false,
+        options: '',
+        create: function (input) {
+            return {
+                value: input,
+                text: input
             }
-
-            settings = $.extend(defaults, settings);
-            return this.each(function () {
-                var options = $(this).data('options'),
-                    _settings = {};
-                if (typeof options !== "undefined") {
-                    _settings.options = options;
-                }
-                _settings = $.extend(settings, _settings);
-                _settings.options = buildOptions(_settings.options);
-                $(this).selectize($.extend(settings, _settings));
-            });
         }
-    });
+    };
+
+    function buildOptions(options) {
+        var opts = [];
+        $.each(options.split(','), function (i, value) {
+            opts.push({
+                value: value,
+                text: value
+            });
+        });
+        return opts;
+    }
+
+    function SelectizeTags(element, settings) {
+        this.element = $(element);
+        this.settings = $.extend(true, defaults, settings);
+        this.init();
+    }
+
+    SelectizeTags.prototype = {
+        init: function () {
+            var options = this.element.data('options');
+            if (options) {
+                this.settings.options = options;
+            }
+            this.settings.options = buildOptions(this.settings.options);
+            this.element.selectize(this.settings);
+        }
+    };
+
+    $.fn.selectize_tags = function (options) {
+        var lists = this,
+            retval = this;
+        lists.each(function () {
+            var plugin = $(this).data("selectize_tags");
+
+            if (!plugin) {
+                $(this).data("selectize_tags", new SelectizeTags(this, options));
+            } else {
+                if (typeof options === 'string' && typeof plugin[options] === 'function') {
+                    retval = plugin[options]();
+                }
+            }
+        });
+
+        return retval || lists;
+    };
 })(jQuery);
