c07408d991b122bdfc519dcaa86ec87cd0b06a99
Minh Bang <contact@minhbang.com>
2015-10-17 16:57:39 +0700

    update dropzone manage

--- jquery.dropzone.manage.js
+++ jquery.dropzone.manage.js
@@ -1,3 +1,8 @@
+/**
+ * Quản lý Dropzone Js
+ * Sử dụng:
+ * $(selector).mbDropzone(options);
+ */
 ;
 (function ($) {
     'use strict';
@@ -10,13 +15,91 @@
         thumb_height: 360,
         token: null,
         trans: {
+            title: 'Tiêu đề',
             null_title: '— Không tiêu đề —',
             delete_title: 'Xóa Hình ảnh',
             delete_confirm: 'Bạn có chắc chắn muốn xóa Hình ảnh này?',
             ok: 'Đồng ý',
-            cancel: 'Bỏ qua'
+            cancel: 'Bỏ qua',
+            delete: 'Xóa',
+            upload: 'Tải lên',
+            add_files: 'Thêm File',
+            tags: 'Tags'
         }
     };
+
+    function templateQuickUpdate(attr, cls, title, plt, label) {
+        var data = {attr: attr, title: title, class: "w-md", placement: plt, value: ""},
+            data_qu = '';
+        $.each(data, function (name, value) {
+            data_qu += ' data-qu_' + name + '="' + value + '"';
+        });
+        return '<a href="#" class="' + cls + ' quick-update text-gray"' + data_qu + 'title="' + title + '" data-toggle="tooltip">' + label + '</a>';
+    }
+
+    function templateButton(type, name, title, icon) {
+        return '<a href="#" class="btn btn-' + type + ' btn-xs ' + name + '" data-toggle="tooltip" title="' + title + '">' +
+            '<i class="fa fa-' + icon + '"></i>' +
+            '</a>';
+    }
+
+    function templatePreview(trans) {
+        return '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">' +
+            '<div class="inner">' +
+            '<div class="status-mark" style="display: none">' +
+            '<span class="dz-error-mark fa fa-remove error"></span>' +
+            '<span class="dz-error-mark fa fa-check success"></span>' +
+            '<div class="bg"></div>' +
+            '</div>' +
+            '<div class="alert alert-danger" data-dz-errormessage style="display: none"></div>' +
+            '<div class="preview"><img data-dz-thumbnail/><div class="sending"></div></div>' +
+            '<div class="form">' +
+            '<div class="input-group">' +
+            templateQuickUpdate('title', 'a-title form-control input-sm img-title', trans.title, 'top', trans.null_title) +
+            '<span class="input-group-addon">' +
+            templateQuickUpdate('tags', 'a-tags', trans.tags, 'bottom', '<i class="fa fa-tags"></i> <span class="badge badge-danger"></span>') +
+            '</span>' +
+            '</div>' +
+            '<input type="hidden" class="img-title" value=""/>' +
+            '<input type="hidden" class="img-tags" value=""/>' +
+            '</div>' +
+            '<div class="actions">' +
+            '<div class="left">' +
+            templateButton('primary', 'start', trans.upload, 'upload') +
+            templateButton('warning', 'cancel', trans.cancel, 'remove') +
+            templateButton('danger', 'delete', trans.delete, 'trash') +
+            '</div>' +
+            '<div class="right">' +
+            '<div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">' +
+            '<div class="progress-bar progress-bar-success" style="width:0;" data-dz-uploadprogress></div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+    }
+
+    function templateContainer(trans) {
+        return '<div class="ibox">' +
+            '<div class="ibox-title">' +
+            '<div class="row buttons">' +
+            '<div class="col-sm-7">' +
+            '<a href="#" class="btn btn-success btn-xs fileinput-button"><i class="fa fa-plus"></i> ' + trans.add_files + '</a>' +
+            '<a href="#" class="btn btn-primary btn-xs start"><i class="fa fa-upload"></i> ' + trans.upload + '</a>' +
+            '<a href="#" class="btn btn-warning btn-xs cancel"><i class="fa fa-remove"></i> ' + trans.cancel + '</a>' +
+            '</div>' +
+            '<div class="col-sm-5">' +
+            '<span class="fileupload-process">' +
+            '<div class="dropzone-total-progress progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">' +
+            '<div class="progress-bar progress-bar-success" style="width:0;" data-dz-uploadprogress></div>' +
+            '</div>' +
+            '</span>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '<div class="ibox-content"><div class="row dropzone-previews"></div></div>' +
+            '</div>';
+    }
 
     function MbDropzone(element, options) {
         this.element = $(element);
@@ -26,13 +109,8 @@
 
     MbDropzone.prototype = {
         init: function () {
-            var previewNode = $(".template", this.element);
-            previewNode.removeClass('template');
-            previewNode.attr('id', null);
-            previewNode.attr('style', null);
-            var previewTemplate = previewNode.parent().html();
-            previewNode.remove();
-
+            this.element.html(templateContainer(this.options.trans));
+            
             this.totalBytes = 0;
             this.totalBytesSent = 0;
             this.totalProgress = $('.dropzone-total-progress', this.element);
@@ -43,7 +121,7 @@
                 thumbnailWidth: this.options.thumb_width,
                 thumbnailHeight: this.options.thumb_height,
                 parallelUploads: 1,
-                previewTemplate: previewTemplate,
+                previewTemplate: templatePreview(this.options.trans),
                 autoQueue: false,
                 previewsContainer: ".dropzone-previews",
                 clickable: ".fileinput-button"
