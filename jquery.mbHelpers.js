697d87f2e026369da5f1b6b17b90a4d7904e2541
Minh Bằng <contact@minhbang.com>
2017-07-11 17:27:12 +0700

    Hoàn thiện tính năng modal

--- jquery.mbHelpers.js
+++ jquery.mbHelpers.js
@@ -26,7 +26,7 @@
             var table = $(table_selector);
             var empty_message = $(empty_selector || '#empty_message');
             var out = '';
-            var regex = /\{(:[a-z0-9_]+)(?:\}|\|([a-z0-9\-_\s]+)\})/g;
+            var regex = /{(:[a-z0-9_]+)(?:}|\|([a-z0-9\-_\s]+)})/g;
             var row = '';
             var renderRow = function (key, value) {
                 row = row.replace(':' + key, value);
@@ -52,7 +52,6 @@
          * Tham số
          * - src: url trang nội dung modal => src của frame
          * - container: 'this' | 'parent', mặc định 'this' trong cùng cửa sổ, 'parent': trong cửa sổ cha (button trong modal content)
-         * - id: id của iframe tag
          * - title: tiêu đề modal
          * - icon: icon phía trước tiêu đề
          * - width: null | 'large' | 'small', chiều rộng modal bootstrap
@@ -66,7 +65,6 @@
             const defaults = {
                 src: null,
                 container: 'this',
-                id: 'bootbox-iframe',
                 title: null,
                 icon: null,
                 width: null,
@@ -87,17 +85,21 @@
             var _container = _options.container === 'this' ? window : window.parent;
             var _buttons = $.extend(true, {}, _options.buttons);;
             $.each(_buttons, function(name, button) {
-                _buttons[name].callback = function(e) {
-                    if($(e.target).hasClass('disabled')){
-                        return false;
-                    } else if($.isFunction(_options.buttons[name].callback)){
-                        return _options.buttons[name].callback.call(this, e);
+                if(button === false){
+                    delete _buttons[name];
+                } else {
+                    _buttons[name].callback = function(e) {
+                        if($(e.target).hasClass('disabled')){
+                            return false;
+                        } else if($.isFunction(_options.buttons[name].callback)){
+                            return _options.buttons[name].callback.call(this, e);
+                        }
                     }
                 }
             });
             return _container.bootbox.dialog({
                 title: (_options.icon ? '<i class="fa fa-'+_options.icon+'"></i> ':'') +_options.title,
-                message: '<iframe id="'+_options.id+'" width="100%" height="'+_options.height+'" src="'+_options.src+'" frameborder="0"></iframe>',
+                message: '<iframe width="100%" height="'+_options.height+'" src="'+_options.src+'" frameborder="0"></iframe>',
                 size: _options.width,
                 className: _options.className,
                 buttons: _buttons,
@@ -106,113 +108,81 @@
         },
         /**
          * Show modal, tham số cung cấp qua tag <a>
-         * vd: <a class="modal-link" data-title="Test modla" data-icon="floppy-disk" data-label="OK" href="/index.html">Show</a>
-         * Tham số gồm:
-         * - href: link đến trang nội dung modal => src của frame
-         * - data-container: 'this' | 'parent', mặc định 'this' trong cùng cửa sổ, 'parent': trong cửa sổ cha (button trong modal content)
-         * - data-title: tiêu đề modal
-         * - data-icon: icon phía trước tiêu đề
-         * - data-width: chiều rộng iframe, px
-         * - data-height: chiều cao iframe, px
-         * - data-submit: label của nút submit, bỏ qua => ẩn nút
+         * vd: <a data-title="Test modla" data-icon="floppy-disk" data-label="OK" href="/index.html">Show</a>
+         * Các tham số cơ bản lấy qua data-* attributes, truyền cho showBootbox()
+         * Riêng:
+         * - href: là src của frame
+         * - data-label: label của nút submit, bỏ qua => chế độ show message
          * - data-callback: khi submit sẽ gọi hàm window[<callback>] thay vì submit form
-         * Trả về: Jquery obj kết quả của bootbox.dialog()
          */
-        showModal: function (el) {
-            var dialog_class = {'large': 'modal-lg', 'small': 'modal-sm'};
-            var data = $(el).data();
-            var modal = $(data.target || '#mbModal');
-            var dialog = modal.children('.modal-dialog');
-            var content = dialog.children('.modal-content');
-            var header = content.children('.modal-header').children('h4');
-            var iframe = content.children('.modal-body').children('iframe');
-            var footer_form = content.children('.modal-footer.for-form');
-            var footer_info = content.children('.modal-footer.for-info');
-            var note;
-            var submit = footer_form.children('.submit');
-
-            // modal title
-            if (data.title)
-                header.children('span').html(data.title);
-            if (data.icon) {
-                if (data.icon.substr(0, 3) === 'fa-') {
-                    header.children('i').attr('class', 'fa ' + data.icon);
-                } else {
-                    header.children('i').attr('class', 'glyphicon glyphicon-' + data.icon);
+        showModal: function (element) {
+            var options = $(element).data();
+            options.src = $(element).attr('href');
+            if(options.label){
+                options.buttons = {
+                    confirm: {
+                        label: options.label,
+                        className: 'btn-success',
+                    },
+                    cancel: {
+                        label: window.trans.cancel
+                    }
+                }
+                if(options.callback){
+                    options.buttons.confirm.callback = window[options.callback] ? window[options.callback] : null
+                } else{
+                    options.buttons.confirm.callback = function() {
+                        $('form', $('iframe', this).contents()).submit();
+                        return false;
+                    }
+                }
+            } else{
+                options.buttons = {
+                    confirm: {
+                        label: window.trans.close,
+                        className: 'btn-success'
+                    },
+                    cancel: false
                 }
             }
-
-            // modal height
-            if (data.height)
-                iframe.height(data.height);
-            else
-                iframe.height('auto');
-
-            // modal width
-            dialog.attr('class', 'modal-dialog');
-            if (data.width && dialog_class[data.width]) {
-                dialog.addClass(dialog_class[data.width]);
-            }
-
-            // modal footer
-            if (data.label) {
-                note = footer_form.children('.note');
-
-                submit.children('span').html(data.label);
-                footer_form.show();
-                footer_info.hide();
-            } else {
-                footer_form.hide();
-                footer_info.show();
-                note = footer_info.children('.note');
-            }
-
-            // footer note
-            if (data.note) {
-                note.children('span').html($.parseHTML(data.note));
-                note.show();
-            } else
-                note.hide();
-
-            iframe.attr('src', $(el).attr('href'));
-            modal.modal('show');
-
-            if (data.hide_callback) {
-                modal.on('hide.bs.modal', function () {
-                    window[data.hide_callback]();
-                });
-            }
-            else {
-                modal.off('hide.bs.modal');
-            }
-
-            if (data.callback)
-                submit.addClass('callback');
-            else
-                submit.removeClass('callback');
+            this.showBootbox(options);
         },
-        showModalMessage: function (title, message, icon, type) {
-            var modal = $('#mbModal-message');
-            var modal_icon = $('.modal-title i', modal);
-            var modal_title = $('.modal-title span', modal);
-            var modal_message = $('.modal-body h4', modal);
-            modal_title.html(title);
-            modal_message.html(message);
-            if (type)
-                modal_message.attr('class', 'text-' + type);
-            else
-                modal_message.attr('class', '');
-            if (icon) {
-                modal_icon.attr('class', 'glyphicon glyphicon-' + icon);
-                modal_icon.show();
-            }
-            else
-                modal_icon.hide();
-            modal.modal('show');
+        showModalMessage: function (title, message, icon, type, size) {
+            return window.bootbox.dialog({
+                title: (icon ? '<i class="fa fa-'+icon+'"></i> ':'') +title,
+                message: '<h4 class="text-center'+(type ? ' text-'+type:'')+'">'+message+'</h4>',
+                size: size,
+                buttons: {
+                    confirm: {
+                        label: window.trans.close,
+                        className: 'btn-success'
+                    }
+                },
+                onEscape: true
+            });
         },
-        updateModalHeight: function (selector) {
-            selector = selector || "#mbModal";
-            window.parent.$(selector).find('iframe').height($(document).height());
+        updateModalHeight: function () {
+            var iframe = this.getParentIframe();
+            if(iframe){
+                $(iframe).height($(document).height());
+            }
+        },
+        getParentIframe: function(container, win) {
+            container = container || window.parent;
+            win = win || window;
+            var result;
+            $.each(container.$('iframe'), function(i, iframe) {
+                if($(iframe.contentWindow).is($(win))){
+                    result = iframe;
+                }
+            });
+            return result;
+        },
+        getParentModal: function(container, win){
+            var iframe = this.getParentIframe(container, win);
+            if(iframe){
+                return $(iframe).closest('.modal');
+            }
         }
     };
 
